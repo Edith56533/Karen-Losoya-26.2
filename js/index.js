725d1ce237345fa10609dcab7fe81f775af36ca5
Karen Losoya <karenn.losoya@outlook.com>
2026-05-26 21:46:52 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -16,6 +16,32 @@ for (let i = 0; i< skills.length; i++){
     let skill = document.createElement('li')
     skill.innerHTML = skills[i]
     skillsList.appendChild(skill)
-    
 }
+
+let messageForm = document.querySelector('[name="leave_message"]')
+messageForm.addEventListener('submit', function(event){
+event.preventDefault()
+let name = event.target.usersName.value
+let email = event.target.usersEmail.value
+let message = event.target.usersMessage.value
+
+console.log(name, email, message)
+
+let messageList = document.querySelector('#messages ul')
+let newMessage = document.createElement('li')
+newMessage.innerHTML = `<a href = "mailto:${name}">${email}</a> <span>${message}</span>`
+
+messageList.appendChild(newMessage)
+
+let removeButton = document.createElement('button')
+removeButton.innerHTML = 'Remove'
+removeButton.addEventListener('click', function(){
+    let entry = this.parentNode
+    entry.remove()
+})
+
+newMessage.appendChild(removeButton)
+
+messageForm.reset()
+})
 
